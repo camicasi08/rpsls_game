9dc0045b72fbb38df932dcd591a5f8038238806c
Camilo Castillo <camiloandres4@gmail.com>
2020-02-23 20:11:41 -0500

    new prefix for game room

--- rpsls/consumers.py
+++ rpsls/consumers.py
@@ -2,10 +2,10 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
 
-class ChatConsumer(WebsocketConsumer):
+class GameConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = 'game_%s' % self.room_name
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
@@ -72,28 +72,7 @@ class ChatConsumer(WebsocketConsumer):
                 }
             )
 
-        elif 'message' in text_data_json:
-
-            message = text_data_json['message']
-
-            # Send message to room group
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'chat_message',
-                    'message': message
-                }
-            )
-
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
-    
+     
     def opponent_message(self, event):
         opponent = event['opponent']
 
